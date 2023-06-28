0f6d8b5a512a9000ac04b375ebf7e7805a8268f7
suzyhan <129558186+suzyhan@users.noreply.github.com>
2023-06-28 16:39:32 -0600

    Create utils folder for generated logo, create array of questions for user input on logo properties

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
 // Import file system and inquirer
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateLogo = require('./utils/generateLogo');
+
+// Create an array of questions for user input that specifies properties for generated svg logo
+const questions = [
+    // WHEN I am prompted for text
+    // THEN I can enter up to three characters
+    {
+        type: "input",
+        name: "text",
+        message: "Please type in a character set of up to three letters."
+    },
+    // WHEN I am prompted for the text color
+    // THEN I can enter a color keyword (OR a hexadecimal number)
+    {
+        type: "input",
+        name: "textColor",
+        message: "What color would you like the text to be? Enter a color (OR a hexadecimal number)."
+    },
+    // WHEN I am prompted for a shape
+    // THEN I am presented with a list of shapes to choose from: circle, triange, and square
+    {
+        type: "list",
+        name: "shape",
+        message: "Which shape would you like the logo to be?",
+        choices: [
+            "Triangle",
+            "Square",
+            "Circle"
+        ]
+    },
+    // WHEN I am prompted for the shape's color
+    // THEN I am can enter a color keyword (OR a hexadecimal number)
+    {
+        type: "input",
+        name: "shapeColor",
+        message: "What color would you like the shape to be? Enter a color (OR a hexadecimal number)"
+    },
+];
